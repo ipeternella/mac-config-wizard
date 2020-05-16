bd174af76a315c4d0a572c5d7d12d84d49ca3638
IgooorGP <igor.feq@gmail.com>
2020-05-16 18:43:44 -0300

    Improved k8s tools module.

--- wizard/installs/k8s_tools/k8s.sh
+++ wizard/installs/k8s_tools/k8s.sh
@@ -1,5 +1,10 @@
+#------------------------------------------------------------
+# K8s tools such as kubectl.
+# ------------------------------------------------------------
+source ./wizard/settings/settings.sh
+
 read -r -p "[K8S]: Install kubectl? [y/n] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+if [[ "$response" =~ $YES_REGEXP ]]
 then
 
     echo "Installing kubectl to access K8s cluster master endpoint..."
