947a99702e8fe9fcca5b30a8ccd6bd48faa7c449
IgooorGP <igor.feq@gmail.com>
2020-05-16 18:31:29 -0300

    Refactored apps with GUI.

--- wizard/installs/apps/apps.sh
+++ wizard/installs/apps/apps.sh
@@ -1,36 +1,38 @@
-#
-# Apps & Utilities
-#
+#------------------------------------------------------------
+# Applications with GUI such as Chrome, Slack, etc.
+# ------------------------------------------------------------
+source ./wizard/settings/settings.sh
+
 read -r -p "[APPS]: Install Google Chrome? [y/n] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+if [[ "$response" =~ $YES_REGEXP ]]
 then
     echo "Installing Google Chrome..."
     brew cask install google-chrome
 fi
 
 read -r -p "[APPS]: Install Iterm2? [y/n] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+if [[ "$response" =~ $YES_REGEXP ]]
 then
     echo "Installing Iterm2..."
     brew cask install iterm2
 fi
 
 read -r -p "[APPS]: Install Alfred? [y/n] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+if [[ "$response" =~ $YES_REGEXP ]]
 then
     echo "Installing Alfred..."
     brew cask install alfred
 fi
 
 read -r -p "[APPS]: Install Slack? [y/n] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+if [[ "$response" =~ $YES_REGEXP ]]
 then
     echo "Installing Slack with brew..."
     brew cask install slack
 fi
 
 read -r -p "[APPS]: Install Tunnelblick (OpenVPN client)? [y/n] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+if [[ "$response" =~ $YES_REGEXP ]]
 then
     echo "Installing Tunneblick..."
     brew cask install tunnelblick
