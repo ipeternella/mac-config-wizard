f90c237acb7b514023e0c65059e7102978a95dee
IgooorGP <igor.feq@gmail.com>
2020-05-16 18:42:25 -0300

    Improved cloud tools module.

--- wizard/installs/cloud_tools/cloud.sh
+++ wizard/installs/cloud_tools/cloud.sh
@@ -1,5 +1,11 @@
+#------------------------------------------------------------
+# Cloud command line tools such as gcloud and aws cli.
+# Also runs some configurations.
+# ------------------------------------------------------------
+source ./wizard/settings/settings.sh
+
 read -r -p "[CLOUD TOOLS]: Install awscli? [y/n] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+if [[ "$response" =~ $YES_REGEXP ]]
 then
 
     echo "Installing awscli..."
@@ -8,17 +14,12 @@ then
 fi
 
 read -r -p "[CLOUD TOOLS]: Would you like to configure awscli now? [y/n] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+if [[ "$response" =~ $YES_REGEXP ]]
 then
-
+    # "For no token-refreshing authentication for ECR, visit: https://github.com/awslabs/amazon-ecr-credential-helper"
 
     echo "Configuring authentication with AWS Services..."
     aws configure
-
-    echo "Configuring Docker client authentication to access AWS ECR (12 hours only)..."
-    aws ecr get-login
-
-    echo "For no token-refreshing authentication for ECR, visit: https://github.com/awslabs/amazon-ecr-credential-helper"
 
     echo "Installing aws-iam-authenticator to use IAM for EKS cluster authentication..."
     brew install aws-iam-authenticator
@@ -31,11 +32,12 @@ then
     read -r -p "[CLOUD TOOLS]: What is your AWS EKS region?" aws_region
     read -r -p "[CLOUD TOOLS]: What is your EKS cluster name?" eks_cluster_name
     aws eks --region $aws_region update-kubeconfig --name $eks_cluster_name
+
 fi
 
 
 read -r -p "[CLOUD TOOLS]: Install google-cloud-sdk? [y/n] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+if [[ "$response" =~ $YES_REGEXP ]]
 then
 
     echo "Installing google cloud sdk..."
@@ -44,11 +46,17 @@ then
 fi
 
 read -r -p "[CLOUD TOOLS]: Would you like to configure google sdk now? [y/n] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-then        
+if [[ "$response" =~ $YES_REGEXP ]]
+then
+
     echo "Configuring authentication with GCP..."
     gcloud auth login --brief
 
     echo "Configuring Docker client authentication to access GCP registries..."
     gcloud auth configure-docker
+
+    echo "Creating a new kubeconfig for cluster authentication with gcloud cli..."
+    read -r -p "[CLOUD TOOLS]: What is your GKE cluster name?" gke_cluster_name
+    gcloud container clusters get-credentials gke_cluster_name
+
 fi
