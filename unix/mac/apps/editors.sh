e78a473a13707c08b5b9be68ae8b327300f3d40d
Alexandre Xavier <ale.bxsantos@gmail.com>
2021-05-08 18:58:56 -0300

    feat: added code editors module

---
+++ unix/mac/apps/editors.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source unix/utils.sh
+
+# Editors
+vscode_choice=$(check_for_command code "VSCode")
+if [[ "${vscode_choice,,}" =~ ^(yes|y)$ ]]; then
+  brew install --cask visual-studio-code
+fi
+
+pycharm_pe_choice=$(check_for_command pycharm-professional "PyCharm Professional")
+if [[ "${pycharm_pe_choice,,}" =~ ^(yes|y)$ ]]; then
+  brew install --cask pycharm
+fi
+
+if [[ "${pycharm_pe_choice,,}" =~ ^(no|n)$ ]]; then
+  pycharm_ce_choice=$(check_for_command pycharm-community "PyCharm Community")
+  if [[ "${pycharm_ce_choice,,}" =~ ^(yes|y)$ ]]; then
+    brew install --cask pycharm-ce
+  fi
+fi
+
+webstorm_choice=$(check_for_command webstorm "WebStorm")
+if [[ "${webstorm_choice,,}" =~ ^(yes|y)$ ]]; then
+  brew install --cask webstorm
+fi
