f3f4740760dca62e73f7e0b817b37cbeb978e45a
Alexandre Xavier <ale.bxsantos@gmail.com>
2021-05-08 18:56:55 -0300

    feat: added api client module

---
+++ unix/linux/apps/api_client.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source unix/utils.sh
+
+# API client
+postman_choice=$(check_for_command postman "Postman")
+if [[ "${postman_choice,,}" =~ ^(yes|y)$ ]]; then
+  snap install postman
+fi
+
+insomnia_choice=$(check_for_command insomnia "Insomnia")
+if [[ "${insomnia_choice,,}" =~ ^(yes|y)$ ]]; then
+  snap install insomnia
+fi
---
+++ unix/mac/apps/api_client.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source unix/utils.sh
+
+source utils.sh
+
+# API client
+postman_choice=$(check_for_command postman "Postman")
+if [[ "${postman_choice,,}" =~ ^(yes|y)$ ]]; then
+  brew install --cask postman
+fi
+
+insomnia_choice=$(check_for_command insomnia "Insomnia")
+if [[ "${insomnia_choice,,}" =~ ^(yes|y)$ ]]; then
+  brew install --cask insomnia
+fi
