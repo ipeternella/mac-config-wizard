4edd95140e671858a1643e5a1d569b4b09cb8c68
Alexandre Xavier <ale.bxsantos@gmail.com>
2021-05-08 18:59:26 -0300

    feat: added runtime version manager module

---
+++ unix/linux/apps/version_managers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source unix/utils.sh
+
+# Runtime version manager
+asdf_choice=$(check_for_command asdf "asdf")
+if [[ "${asdf_choice,,}" =~ ^(yes|y)$ ]]; then
+  ASDF_VERSION=$(get_latest_tag "asdf-vm/asdf")
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch "$ASDF_VERSION"
+  bash "$HOME"/.asdf/asdf.sh plugin add python
+fi
---
+++ unix/mac/apps/version_managers.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source unix/utils.sh
+
+# Runtime version manager
+asdf_choice=$(check_for_command asdf "asdf")
+if [[ "${asdf_choice,,}" =~ ^(yes|y)$ ]]; then
+  brew install asdf
+fi
