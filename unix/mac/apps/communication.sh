b805d8f0ccc9c9a4492d45fa829a543448900029
Alexandre Xavier <ale.bxsantos@gmail.com>
2021-05-08 18:57:27 -0300

    feat: added communication module

---
+++ unix/mac/apps/communication.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source unix/utils.sh
+
+# Communication
+slack_choice=$(check_for_command slack "Slack")
+if [[ "${slack_choice,,}" =~ ^(yes|y)$ ]]; then
+  brew install --cask slack
+fi
+
+zoom_client_choice=$(check_for_command zoom-client "Zoom Client")
+if [[ "${zoom_client_choice,,}" =~ ^(yes|y)$ ]]; then
+  brew install --cask zoom
+fi
