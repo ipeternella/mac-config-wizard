5b98ae8287ae80f4b46884170cd0d66d8cd815a2
Alexandre Xavier <ale.bxsantos@gmail.com>
2021-05-08 18:59:46 -0300

    feat: added nodejs to programming languages module

---
+++ unix/mac/apps/programming_languages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source unix/utils.sh
+
+# NodeJS
+read -rp "Do you want to install NodeJS LTS? [y/n] " node_choice
+if hash "asdf" &>/dev/null && [[ "${node_choice,,}" =~ ^(yes|y)$ ]]; then
+  asdf plugin list | grep nodejs || asdf plugin add nodejs
+  asdf install nodejs lts
+fi
